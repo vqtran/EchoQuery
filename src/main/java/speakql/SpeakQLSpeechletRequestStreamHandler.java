11c6904f5369ed38a74bafb09b56b5084356e7cb
Vinh Tran <vinh_tran@brown.edu>
2015-10-19 11:51:36 -0400

    Cleaned up samples and fixed style.
    Deleted a bunch of unnecessary stuff. Fixed errors, and fixed Java
    style.

--- src/main/java/speakql/SpeakQLSpeechletRequestStreamHandler.java
+++ src/main/java/speakql/SpeakQLSpeechletRequestStreamHandler.java
@@ -1,12 +1,3 @@
-/**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.amazon.com/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
 package speakql;
 
 import java.util.HashSet;
@@ -16,32 +7,27 @@ import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 
 /**
- * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
- * experience. To do this, simply set the handler field in the AWS Lambda console to
- * "historybuff.HistoryBuffSpeechletRequestStreamHandler" For this to work, you'll also need to
- * build this project using the {@code lambda-compile} Ant task and upload the resulting zip file to
- * power your function.
+ * Request handler for AWS Lambda deployment.
  */
-public class SpeakQLSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+public class SpeakQLSpeechletRequestStreamHandler
+  extends SpeechletRequestStreamHandler {
 
-    private static final Set<String> supportedApplicationIds;
+  private static final Set<String> supportedApplicationIds;
 
-    static {
-        /*
-         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
-         * Alexa Skill and put the relevant Application Ids in this Set.
-         */
-        supportedApplicationIds = new HashSet<String>();
-        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.f705b0c1-42a2-4c94-9e48-50aed83b2310");
-    }
+  // TODO(vqtran): Make application id a system property because developers
+  // probably should not share volatile development instances of Alexa skills.
+  static {
+    supportedApplicationIds = new HashSet<String>();
+    supportedApplicationIds.add(
+        "amzn1.echo-sdk-ams.app.f705b0c1-42a2-4c94-9e48-50aed83b2310");
+  }
 
-    public SpeakQLSpeechletRequestStreamHandler() {
-        super(new SpeakQLSpeechlet(), supportedApplicationIds);
-    }
+  public SpeakQLSpeechletRequestStreamHandler() {
+    super(new SpeakQLSpeechlet(), supportedApplicationIds);
+  }
 
-    public SpeakQLSpeechletRequestStreamHandler(Speechlet speechlet,
-            Set<String> supportedApplicationIds) {
-        super(speechlet, supportedApplicationIds);
-    }
-
+  public SpeakQLSpeechletRequestStreamHandler(
+      Speechlet speechlet, Set<String> supportedApplicationIds) {
+    super(speechlet, supportedApplicationIds);
+  }
 }
