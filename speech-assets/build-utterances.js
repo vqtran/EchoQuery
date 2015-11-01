1adcc047a18e7d86ebf8097a0cbf88a58fb135d0
Vinh Tran <vinh_tran@brown.edu>
2015-11-01 00:52:01 -0400

    Our very own templating engine for utterances.
    So there's this existing templating engine called alexa-utterances. Its
    the only one I can find so far. I tried using it for a while but
    realized that:
    
    1. It has a lot of necessary features for us.
    2. Has an ugly syntax.
    3. Most legit, its syntax plays poorly with custom slot types.
    4. Their code is a mess and would be a pain to edit.
    
    So armed with a six pack of Lagunitas Little Sumpin' Sumpin' I set out
    to roll my own templating engine for these utterances. It's pretty damn
    simple so far, but gets the job done much better than alexa-utterance.
    Here's the only two things this thing does:
    
      [a|b|c] - Definite variation. An utterance will be made for each version
                listed here.
    
      Ex: "Hi [Gabe|Vinh Tran]" => ["Hi Gabe", "Hi Vinh Tran"]
    
      (a|b|c) - Optional variation. An utterance will be made for each version
                listed here, as well as one without.
    
      Ex: "Hi (Gabe|Vinh Tran)" => ["Hi", "Hi Gabe", "Hi Vinh Tran"]
    
    I then rewrote our sample utterances into a much much shorter template!
    With this now making conditionals will be a lot more reasonable.

---
+++ speech-assets/build-utterances.js
@@ -0,0 +1,99 @@
+var fs = require('fs');
+
+// Generate the built file.
+buildTemplate('UtterancesTemplate.txt', 'built/SampleUtterances.txt');
+
+/**
+ * EchoQuery's very own Alexa utterances templating engine. There's a single
+ * other package that does something like this called alexa-utterances. But,
+ * it's pretty cumbersome, has a lot of unnecessary features, weird syntax, and
+ * above all else, syntax that doesn't play nicely with custom slots.
+ *
+ * Here's the only two things this thing does:
+ *
+ * [a|b|c] - Definite variation. An utterance will be made for each version
+ *           listed here.
+ *
+ * Ex: "Hi [Gabe|Vinh]" => ["Hi Gabe", "Hi Vinh"]
+ *
+ * (a|b|c) - Optional variation. An utterance will be made for each version
+ *           listed here, as well as one without.
+ *
+ * Ex: "Hi [Gabe|Vinh]" => ["Hi", "Hi Gabe", "Hi Vinh"]
+ *
+ * TODO(vqtran): Split this out into it's own NPM package and give it a cute
+ *    name once we battle test this a bit.
+ */
+
+function buildTemplate(input, output) {
+  // Read in input template file.
+  fs.readFile(input, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    // Expand each line out into its combinations.
+    var expandedUtterances = '';
+    data.split('\n').forEach(function(line) {
+      expandedUtterances += expandUtterance(line).join('\n') + '\n';
+    });
+
+    // Write all expansions to new output file.
+    fs.writeFile(output, expandedUtterances, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+}
+
+function expandUtterance(line) {
+  // We'll keep a set of expansions that we'll constantly multiply in size as
+  // we run into more variations.
+  var expansions = [''];
+
+  // Run through the line character by character and build the set. This could
+  // probably done with Regex, but this might be easier to extend in the future
+  // if we wanted to include more special template-y features.
+  for (var i = 0; i < line.length; i++) {
+    var current = line.charAt(i);
+
+    if (current == '[') {
+      // If we find a definite variation, grab everything until the next ending
+      // bracket and extend the expansions.
+      var j = i + line.substring(i).indexOf(']');
+      var variations = line.substring(i+1, j).split('|');
+      expansions = extendExpansions(expansions, variations);
+      i = j;
+    } else if (current == '(') {
+      // If we find an optional variation, grab everything until the next ending
+      // paranthesis and concat the new expansions to the existing ones to keep
+      // the optional-ness of it.
+      var j = i + line.substring(i).indexOf(')');
+      var variations = line.substring(i+1, j).split('|');
+      expansions = expansions.concat(extendExpansions(expansions, variations));
+      i = j;
+    } else {
+      // Otherwise if its just a plain character then just expand by that.
+      expansions = extendExpansions(expansions, [current]);
+    }
+  }
+
+  // Clean up any extra whitespaces.
+  expansions.forEach(function(line, i, arr) {
+    arr[i] = line.trim().replace(/\s\s+/g, ' ');
+  });
+
+  return expansions;
+}
+
+// Extend an expansion set by the given variations, makes the new combinations.
+function extendExpansions(expansions, variations) {
+  var newExpansions = [];
+  expansions.forEach(function(expansion) {
+    variations.forEach(function(variation) {
+      newExpansions.push(expansion + variation);
+    });
+  });
+  return newExpansions;
+}
