08e3237470365226648148bbe17206f026552749
Gabe Lyons <gabriel_lyons@brown.edu>
2016-01-05 00:11:29 -0500

    checking out what this diff is gonna look like

--- src/main/resources/js/stores/SessionStore.js
+++ src/main/resources/js/stores/SessionStore.js
@@ -1,22 +1,40 @@
 import alt from '../alt';
+
 import DisplayActions from '../actions/DisplayActions';
+import SessionActions from '../actions/SessionActions';
 
 class SessionStore {
 
   constructor() {
     this.bindListeners({
       updateDisplayText: DisplayActions.setDisplayText,
+      updateSessionId: SessionActions.setSessionId,
     });
 
     this.state = {
       displayText: "What do you want?",
+      userId: 0,
     };
+
+    this.exportPublicMethods({
+      getSessionId: this.getSessionId,      
+    });
   }
 
   updateDisplayText(data) {
     this.setState({
       displayText: data,
     });
+  }
+
+  updateSessionId(data) {
+    this.setState({
+      userId: data,
+    });
+  }
+
+  getSessionId() {
+    return this.state.userId;
   }
 }
 
